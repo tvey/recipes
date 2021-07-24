4ada25c32e6d168376f2b8ea6f800783a682ea15
tvey <tanyavoid@ya.ru>
2021-07-24 17:14:09 +0300

    Remove unused imports

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import (
-    LogoutView,
-    PasswordResetView,
-    PasswordResetDoneView,
-    PasswordResetConfirmView,
-    PasswordResetCompleteView,
-)
+from django.contrib.auth.views import LogoutView
 
 from .views import (
     register,
